923e6acc1f288edf7949d88f1751b37cd9ea8b58
ArdaOzcanCifci <250542012@firat.edu.tr>
2025-10-23 20:14:53 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,19 @@
+
+public class IsimKarti {
+    public static void main(String[] args){
+        //öğrenci bilgileri
+        String ad = "Ahmet";
+        String soyad = "Yilmaz";
+        String ogrNo = "220301010";
+        String bolum = "Yazilim Muh.";
+        //yazdırma
+        System.out.println("===================");
+        System.out.println(" OGRENCI BILGIERI");
+        System.out.println("===================");
+        System.out.println("Ad : " + ad);
+        System.out.println("Soyad : " + soyad);
+        System.out.println("No : " + ogrNo);
+        System.out.println("Bolum : " + bolum);
+        System.out.println("===================");
+    }
+}
