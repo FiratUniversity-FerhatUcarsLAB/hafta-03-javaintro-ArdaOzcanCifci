6e82541e8ff86ee81f552d492fdd860ddf1cf53d
ArdaOzcanCifci <250542012@firat.edu.tr>
2025-10-23 20:11:31 +0300

    Add files via upload

---
+++ TASK-BONUS/AcsiiArtIsim.java
@@ -0,0 +1,40 @@
+public class JavaDeseniIPTAL {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("     AAAAAA ");
+        System.out.println("    A A  A A");
+        System.out.println("   A A    A A");
+        System.out.println("  AAAAAAAAAAAA ");
+        System.out.println("  A A      A A");
+        System.out.println("  A A      A A");
+        System.out.println(); //boşluk için
+
+        System.out.println("  RRRRRRRRRR");
+        System.out.println("  R       R");
+        System.out.println("  R     R");
+        System.out.println("  R R R");
+        System.out.println("  R    R ");
+        System.out.println("  R     R");
+        System.out.println(); // Boşluk için
+
+
+
+        System.out.println("  DDDDDDDD ");
+        System.out.println("  D       D ");
+        System.out.println("  D        D ");
+        System.out.println("  D        D");
+        System.out.println("  D       D");
+        System.out.println("  DDDDDDDD");
+        System.out.println(); //boşluk için
+
+
+        System.out.println("     AAAAAA ");
+        System.out.println("    A A  A A");
+        System.out.println("   A A    A A");
+        System.out.println("  AAAAAAAAAAAA ");
+        System.out.println("  A A      A A");
+        System.out.println("  A A      A A");
+    }
+}
